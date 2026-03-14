cf0f8ae2db02eff960c49349c37e27ce3cd38c53
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2026-03-14 21:15:53 +1100

    init commit

---
+++ internal/products/handlers.go
@@ -0,0 +1,31 @@
+package products
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/QuanNguyenDong/ecommerce-api/internal/json"
+)
+
+type handler struct {
+	service Service
+}
+
+func NewHandler(service Service) *handler {
+	return &handler{
+		service: service,
+	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+	err := h.service.ListProducts(r.Context())
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	products := []string{"Hello", "World"}
+
+	json.Write(w, http.StatusOK, products)
+}
